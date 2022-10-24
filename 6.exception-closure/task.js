c4ef14d52af0b74615a96a2e74ee8446af2e0ecf
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-10-24 18:17:55 +0300

    task 6.x - 1.1, 1.2, 2.1 - ok, 2.2 - not yet

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,59 @@
+function parseCount(number) {
+    let answer = Number.parseInt(number);
+    if (isNaN(answer)) {
+        throw new Error("Невалидное значение");
+    } else {
+        return answer;
+    }
+}
+
+function validateCount(number) {
+    try {
+        return parseCount(number);
+    } catch (error) {
+        return error
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (((this.a + this.b) <= this.c) ||
+            ((this.a + this.c) <= this.b) ||
+            ((this.b + this.c) <= this.a)) {
+            this.result = new Error("Треугольник с такими сторонами не существует");
+            throw this.result;
+        }
+    }
+
+    getPerimeter() {
+        if (this.result !== Error) {
+            return this.a + this.b + this.c;
+        }
+        throw new Error("Ошибка! Треугольник не существует");
+    }
+
+    getArea() {
+        if (this.result !== Error) {
+            let semiPerimeter = this.getPerimeter() / 2;
+            return Number.parseFloat(Math.sqrt((semiPerimeter *
+                (semiPerimeter - this.a) *
+                (semiPerimeter - this.b) *
+                (semiPerimeter - this.c))).toFixed(3));
+        }
+        throw new Error("Ошибка! Треугольник не существует");
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        let triangle = new Triangle(a, b, c);
+        triangle.getArea();
+        triangle.getPerimeter();
+    }
+}
